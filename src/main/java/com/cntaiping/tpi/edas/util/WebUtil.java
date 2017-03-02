5f1dc827bcd3aab4398420dcaf12036f3e15ef4a
vidy.tu <vidy.tu@outlook.com>
2017-03-02 09:57:40 +0800

    模板资源调整到templates下

--- src/main/java/com/cntaiping/tpi/edas/util/WebUtil.java
+++ src/main/java/com/cntaiping/tpi/edas/util/WebUtil.java
@@ -13,7 +13,7 @@ public final class WebUtil {
 	
 	public static final String PAGE_ACTION="_PAGE_ACTION";
 	public static final String PAGE_ACTION_CLASS="_PAGE_ACTION_CLASS";
-	public static final String WEB_ROOT="/com/cntaiping/tpi/web";
+	public static final String WEB_ROOT="/templates";
 	public static final String WEB_PKG_ROOT="com.cntaiping.tpi.web.";
 	public static final int WEB_PKG_ROOT_LEN=WEB_PKG_ROOT.length();
 	
